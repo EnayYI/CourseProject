1015a80eb1a691eb98dbd8569e0ca608602d2166
EnayYI <yariklo835@gmail.com>
2022-09-21 22:55:29 +0300

    Add variants of user's choice and choice of computer

--- RSP.py
+++ RSP.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+rsp = ["rock", "scissors", "paper"]
+rsp_randomise = random.choice(rsp)
+user_choice = input("Enter your choice: Rock, Scissors or Paper ")
 
+if user_choice.lower() == rsp_randomise:
+    print("Draw!")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+elif user_choice.lower() == "rock":
+    if rsp_randomise == "scissors":
+        print("You win!")
+    else:
+        print("You lose(")
 
+elif user_choice.lower() == "scissors":
+    if rsp_randomise == "paper":
+        print("You win!")
+    else:
+        print("You lose(")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+elif user_choice.lower() == "paper":
+    if rsp_randomise == "rock":
+        print("You win")
+    else:
+        print("You lose(")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
